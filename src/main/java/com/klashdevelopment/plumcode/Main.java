c37d2f0afe7b75bd8c55d2116b79b03e7c47fbd0
GavinGoGaming <57292172+GavinGoGaming@users.noreply.github.com>
2023-01-21 17:59:39 -0800

    Update Main.java

--- src/main/java/com/klashdevelopment/plumcode/Main.java
+++ src/main/java/com/klashdevelopment/plumcode/Main.java
@@ -3,6 +3,6 @@ package com.klashdevelopment.plumcode;
 public class Main {
 
     public static void main(String[] args) {
-        Plumcode.parse("run other file.plum");
+        Plumcode.parse(String.join(" ", args));
     }
 }
